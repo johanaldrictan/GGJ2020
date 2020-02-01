ee81e82fb117ad75377feb8436bfa651d20b45fb
Roger <roger@dalkefamily.com>
2020-02-01 12:00:24 -0800

    Added Blink animation for slime
    Added an animation controller and blink animation for slime. Added script to randomize blinks.

---
+++ Assets/Scripts/Blinker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Blinker : MonoBehaviour
+{
+    private Animator animCont;
+
+    public float blinkInterval;
+
+    private void Start()
+    {
+        animCont = GetComponent<Animator>();
+        InvokeRepeating("DoBlink", blinkInterval*1.5f, blinkInterval);
+    }
+
+    private void DoBlink()
+    {
+        float rand = Random.value;
+        if (rand < 0.25)
+        {
+            animCont.SetTrigger("Blink");
+        }
+    }
+}
