d55605dd2c3e921312d0f692d1a6edb39795e51f
Ulleseit <ulleseit99@gmail.com>
2020-02-01 12:46:11 -0800

    Player explosion
    Added breaking to player character, explodes into multiples spheres, set velocity of unit vector*10, + the current velocity of the player at explosion time

---
+++ Assets/Scripts/AnchorSphere.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnchorSphere : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.position = new Vector3(this.gameObject.transform.parent.position.x,this.gameObject.transform.parent.position.y - .2f, this.gameObject.transform.parent.position.z);
+    }
+}
---
+++ Assets/Scripts/LocktoXY.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocktoXY : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x,this.gameObject.transform.position.y, 0);
+    }
+}
