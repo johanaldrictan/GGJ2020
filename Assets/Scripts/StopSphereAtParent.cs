6b5efbcf32f559adeefb7ccf7d59ab0220c99e8d
Ulleseit <ulleseit99@gmail.com>
2020-02-01 17:08:27 -0800

    Push to save code
    Reform code save

---
+++ Assets/Scripts/StopSphereAtParent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopSphereAtParent : MonoBehaviour
+{
+    GameObject parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = GameObject.FindWithTag("Parent");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(parent.transform.position.x-.5 <= this.gameObject.transform.position.x || this.gameObject.transform.position.x < parent.transform.position.x+.5)
+        {
+            this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+        }
+    }
+}
